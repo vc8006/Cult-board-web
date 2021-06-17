09276faeb7a3b5ce6a0d85eb43dcfbd35ecaa856
vikas-4402 <68778119+vikas-4402@users.noreply.github.com>
2021-06-17 23:13:42 +0530

    Removed from Main

--- club/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post,Photo,Member
-# Register your models here.
-
-admin.site.register(Post)
-admin.site.register(Photo)
-admin.site.register(Member)
