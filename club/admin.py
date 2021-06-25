bf6da6b9f5a8f0f85db9cd10a71a5091a86c1f46
scarymen <46341830+scarymen@users.noreply.github.com>
2021-06-25 20:24:27 +0530

    Delete club directory

--- club/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Event, Team, Note
-
-admin.site.register(Event)
-admin.site.register(Team)
-admin.site.register(Note)
-
-# Register your models here.
--- club/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns =[
-path('',views.index,name='index'),
-path('culbEvents/',views.ClubEventsView.as_view(),name='clubEvents'),
-path('clubEvent/<int:pk>',views.EventDetailView.as_view(),name='event-detail'),
-]
